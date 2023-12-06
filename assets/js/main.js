8dd9dad5463223eee642dc8535a24822a9c905a1
papayeah <spocifyy@gmail.com>
2023-12-06 08:00:56 -0800

    Changed from blue to neutral, added landing page scroll and nav hover interactions, simplified nav, condensed opening statement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-// document.querySelector("#top").style.filter
+// blur top text
+window.addEventListener('scroll', (e) => {
+    let wTop   = this.scrollY;
+    let mTop   = document.querySelector('main').offsetTop;
+    let open   = document.querySelector('.open');
+    let delta  = (wTop / mTop) * 39;
+
+    if(wTop === mTop) return;
+
+    open.style.filter = "blur(" + Math.trunc(delta) + "px)";
+});
